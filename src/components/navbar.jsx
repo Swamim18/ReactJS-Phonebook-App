15de26141d79f2c0e6062fa5db40e3810ad8e267
sakhayadeep <sakhayadeepnath@gmail.com>
2018-12-19 15:01:29 +0530

    Added bootstrap to make it look good

---
+++ src/components/navbar.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-md navbar-light bg-light rounded">
+        <a className="navbar-brand" href="#">
+          React Phonebook Application
+        </a>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
